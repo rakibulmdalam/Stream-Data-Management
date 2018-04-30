51507cc5a5a01497473d790f77e805908c328482
Rakib <ga84doy@mytum.de>
2018-05-01 01:10:44 +0200

    kafka server start modified

--- kafka_installer.sh
+++ kafka_installer.sh
@@ -10,9 +10,9 @@ then
     then
       wget http://apache.lauf-forum.at/kafka/1.1.0/kafka_2.12-1.1.0.tgz
       tar -xvf kafka_2.12-1.1.0.tgz
-      cd ./kafka_2.12-1.1.0
       echo "starting kafka server..."
-      ~/kafka_2.12-1.1.0/bin/kafka-server-start.sh ~/kafka_2.12-1.1.0/config/server.properties
+      cd ./kafka_2.12-1.1.0
+      ./bin/kafka-server-start.sh ./config/server.properties
     else
       echo "kafka default port [9092] is in use"
     fi
