44247fc07ba6b5e404f0c66f34b17b874134f8b1
Rakib <ga84doy@mytum.de>
2018-05-01 20:34:36 +0200

    converted random temperature value to bytes to avoid Assertion Error

--- kafka-producer.py
+++ kafka-producer.py
@@ -6,7 +6,7 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
 # Asynchronous by default
 temperature = random.uniform(-10, 39)
-future = producer.send('temperature', temperature)
+future = producer.send('temperature', bytes(temperature, "utf-8"))
 
 for i in range(10):
     try:
