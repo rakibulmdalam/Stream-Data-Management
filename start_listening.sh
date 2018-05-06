01959da022f7773ccf0c92d0b2bce352ac0b1289
Rakib <ga84doy@mytum.de>
2018-05-07 01:03:22 +0200

    several updates

--- start_listening.sh
+++ start_listening.sh
@@ -1,16 +1,16 @@
 # start elasticsearch
-sudo service elasticsearch start
+nohup sudo service elasticsearch start
 # start kibana
-sudo systemctl start kibana.service
+nohup sudo systemctl start kibana.service
 
 # start zookeeper
-sudo service zookeeper status
+nohup sudo service zookeeper start
 
 # start kafka
-~/iot/kafka_2.12-1.1.0/bin/kafka-server-start.sh ~/iot/kafka_2.12-1.1.0/config/server.properties
+nohup ~/iot/kafka_2.12-1.1.0/bin/kafka-server-start.sh ~/iot/kafka_2.12-1.1.0/config/server.properties
 
 # start flink
-~/iot/flink-1.4.1/bin/start-local.sh
+nohup ~/iot/flink-1.4.1/bin/start-local.sh
 
 # open tunnel: kafka-flink-elasticsearch
-~/iot/flink-1.4.1/bin/flink run tunnel_kafka_flink_elastic.jar
+nohup ~/iot/flink-1.4.1/bin/flink run streamtunnel-0.1.jar
